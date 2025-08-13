36607ca394d5c95c5dfa7cb7351ea1ab52532009
Áliton Vieira <alitonvieira971@gmail.com>
2025-08-13 10:36:10 +0000

    Adicionando movimentação do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -8,6 +8,7 @@ int main() {
     {
         printf("TORRE PARA DIREITA\n", torre); //imprimindo movimentos da torre
     }
+        printf("\n"); //criando espaço entre a impressão da movimentação das peças
 
     //criando movimentação da rainha
     int rainha = 0;
@@ -17,7 +18,8 @@ int main() {
         printf("RAINHA PARA ESQUERDA\n", rainha); //imprimindo movimentação da rainha
         rainha++; //evitando looping infinito
     }
-    
+        printf("\n"); //criando espaço entre a impressão da movimentação das peças
+
     //criando movimentação do bispo
     int bispo = 0;
 
@@ -28,9 +30,19 @@ int main() {
         bispo++; //evitando looping infinito
 
     } while (bispo <= 4);
+        printf("\n"); //criando espaço entre a impressão da movimentação das peças
+
+    //criando movimentação do cavalo
+    int movimento = 1; // controle de movimentação
+
+    while(movimento--)
+    {
+        for(int cavalo = 0; cavalo < 2; cavalo++)
+        {
+            printf("CAVALO PARA BAIXO\n"); //declaro movimento do cavalo par baixo 2x
+        }
+        printf("CAVALO PARA ESQUERDA\n"); //declaro movimento do cavalo para esquerda
+    }
     
-
-
-
     return 0;
 }
